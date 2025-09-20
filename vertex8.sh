aa1254988efaf463b09ae28ada3d6e5b2870b641
gaserjuk <gaserjukilo@gmail.com>
2025-09-20 16:37:40 +0700

    Update vertex8.sh

--- vertex8.sh
+++ vertex8.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd vertex && chmod 777 run.sh && nproc --all && ./run.sh 8 >/dev/null 2>&1 &
+cd vertex && chmod 777 run.sh && nproc --all && ./run.sh 7 >/dev/null 2>&1 &
 sleep 10
 while true
 do
